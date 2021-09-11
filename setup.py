6b135a352af3f2654aa2d5f8ecc3ae2a50a63ca3
Zhengyi Peng <33950628+pengzhengyi@users.noreply.github.com>
2021-09-11 03:37:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="Streamlined",
-    version="0.1.0",
+    version="0.1.1",
     author="Zhengyi Peng",
     author_email="pengzhengyipengzhengyi@gmail.com",
     description="Make ML or generic pipeline more streamlined",
