84602778f8c2f83822c8c467ff119f48e6989f2e
pengzhengyi <pengzhengyipengzhengyi@gmail.com>
2021-11-17 16:24:43 +0800

    test decorators in reaction is preserving signature

--- tests/unit_testing/ray/test_reaction.py
+++ tests/unit_testing/ray/test_reaction.py
@@ -1,6 +1,8 @@
+import inspect
+
 import pytest
 
-from streamlined.ray.services import raises
+from streamlined.ray.services import after, before, raises
 
 
 def test_raises_return_default_value_at_expected_exception():
@@ -14,3 +16,11 @@ def test_raises_return_default_value_at_expected_exception():
         error_throwing_func
     )
     assert error_caught_func()
+
+
+def test_preserve_signature():
+    def add(x: int, y: int) -> int:
+        return x + y
+
+    transformed_add = after()(before()(raises()(add)))
+    assert inspect.signature(transformed_add) == inspect.signature(add)
