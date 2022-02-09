05b901c462aa993c5d842a060e4bbcd347b9df16
Zhengyi Peng <33950628+pengzhengyi@users.noreply.github.com>
2022-02-09 02:52:50 -0500

    bugfix: fix partial async function not working due to FunctionMaker.create function not handling edge case properly (#36)

--- streamlined/common/functionmaker.py
+++ streamlined/common/functionmaker.py
@@ -1,4 +1,7 @@
 import inspect
+import re
+from asyncio import iscoroutinefunction
+from functools import partial
 from itertools import chain
 from typing import Any, Callable, TypeVar
 
@@ -48,6 +51,65 @@ def rewrite_function_parameters(
     )
 
 
+def create(obj, body, evaldict, defaults=None, doc=None, module=None, addsource=True, **attrs):
+    """
+    Create a function from the strings name, signature and body.
+    evaldict is the evaluation dictionary. If addsource is true an
+    attribute __source__ is added to the result. The attributes attrs
+    are added, if any.
+
+    Note
+    ------
+
+    Same as the `FunctionMaker.create` except that classmethod fail to
+    account for edge case where an async function body contains `return`.
+
+    For example:
+
+    ```
+    async def IDENTITY(return_value):
+        return return_value
+
+
+    FunctionMaker.create('WILL_RAISE_SYNTAX_ERROR()', 'return IDENTITY(return_value=actual_return_value)', dict(IDENTITY=IDENTITY, _call_=IDENTITY, actual_return_value=10), addsource=True)
+    ```
+
+    Will raise the following error:
+
+    ```
+    Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>
+        File "/opt/conda/lib/python3.9/site-packages/decorator.py", line 196, in create
+            return self.make(body, evaldict, addsource, **attrs)
+        File "/opt/conda/lib/python3.9/site-packages/decorator.py", line 159, in make
+            code = compile(src, filename, 'single')
+        File "<decorator-gen-4>", line 2
+            return await IDENTITY(return await_value=actual_return await_value)
+                                ^
+        SyntaxError: invalid syntax
+
+    To fix, we need to match on 'return ' rather than 'return'
+    ```
+    """
+    if isinstance(obj, str):  # "name(signature)"
+        name, rest = obj.strip().split("(", 1)
+        signature = rest[:-1]  # strip a right parens
+        func = None
+    else:  # a function
+        name = None
+        signature = None
+        func = obj
+    self = FunctionMaker(func, name, signature, defaults, doc, module)
+    ibody = "\n".join("    " + line for line in body.splitlines())
+    caller = evaldict.get("_call_")  # when called from `decorate`
+    if caller and iscoroutinefunction(caller):
+        rawbody = "async def %(name)s(%(signature)s):\n" + ibody
+        body = re.sub(r"(^|\s)return(\s)", r"\1return await\2", rawbody)
+    else:
+        body = "def %(name)s(%(signature)s):\n" + ibody
+    return self.make(body, evaldict, addsource, **attrs)
+
+
 def bound(function: Callable[..., T], *args: Any, **kwargs: Any) -> Callable[[], T]:
     """
     Similar to [`functools.partial`](https://docs.python.org/3/library/functools.html#functools.partial), except a new function is created with `exec`.
@@ -58,6 +120,10 @@ def bound(function: Callable[..., T], *args: Any, **kwargs: Any) -> Callable[[],
     """
     if inspect.isfunction(function):
         function_newname = f"bound_{function.__name__}"
+    elif isinstance(function, partial):
+        # rebound a partially bound function such that it is fully bound
+        function = function.func
+        function_newname = f"rebound_{function.__name__}"
     else:
         function_newname = f"call_{function.__class__.__name__}"
         function = function.__call__
@@ -73,7 +139,7 @@ def bound(function: Callable[..., T], *args: Any, **kwargs: Any) -> Callable[[],
         f"_bound_kwarg_{param_name}_value_": param_value
         for param_name, param_value in kwargs.items()
     }
-    return FunctionMaker.create(
+    return create(
         f"{function_newname}()",
         f"return function({parameters})",
         dict(function=function, _call_=function, **arg_values, **kwarg_values),
